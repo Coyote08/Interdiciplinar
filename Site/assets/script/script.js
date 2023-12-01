3a2aa202b3015a8bde823fd355b7dc07792bd930
Deyvid098 <deyvid.davi32@gmail.com>
2023-12-01 10:04:22 -0300

    Arrumado os endereços nos arquivos html

--- Site/assets/script/script.js
+++ Site/assets/script/script.js
@@ -1,13 +1,8 @@
 //variables
 var button_menu = document.getElementById("btn-hamburg");
 var menu_mobile = document.getElementById("nav-mobile");
-var button_languages = document.getElementById("btn-languages");
-var menu_languages = document.getElementById("languages");
 
 //installing listeners
 button_menu.addEventListener("click",function () {
     if (menu_mobile.style.display === "none" || menu_mobile.style.display === "") {menu_mobile.style.display = "flex"} else {menu_mobile.style.display = "none"};
-});
-button_languages.addEventListener("click", function() {
-    if (menu_languages.style.display === "none" || menu_languages.style.display === "") {menu_languages.style.display = "flex"} else {menu_languages.style.display = "none"};
 });
